84635620ef0874c5c5d34fdd5d143aec3b1193aa
nooralden <nooraldenberkdar80@gmail.com>
2024-05-24 17:01:07 +0300

    first commit

---
+++ data/index.ts
@@ -0,0 +1,205 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Deaf-Mute type application",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Food Delivery Application",
+    des: "Explore the wonders of our E-commerce Food application that mange flow of every details .",
+    img: "/p1.svg",
+    iconLists: ["/flutter.svg", "/dart.svg", "/vue.js.svg"],
+    link: "https://github.com/Nooraldenberkdar/Food_Delivery_App",
+  },
+  {
+    id: 2,
+    title: "Supply Chain Application",
+    des: "Simplify your complex trading proccesses in very useful way.",
+    img: "/p2.svg",
+    iconLists: ["/flutter.svg", "/dart.svg"],
+    link: "https://github.com/Nooraldenberkdar/SupplyChain_ClientApp",
+  },
+  {
+    id: 3,
+    title: "climate Auto Detection",
+    des: "A REAL Software-as-a-Service app with weather auto archiver",
+    img: "/p3.svg",
+    iconLists: ["/flutter.svg", "/dart.svg"],
+    link: "https://github.com/Nooraldenberkdar",
+  },
+  {
+    id: 4,
+    title: "Sign Language Translation",
+    des: "Recreated video call application that trasnlate deaf-mute signs using AI",
+    img: "/p4.svg",
+    iconLists: ["/flutter.svg", "/dart.svg"],
+    link: "https://github.com/Nooraldenberkdar",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "كان العمل مع نورالدين بيرقدار تجربة رائعة. كان محترفاً وسريعاً ومتفانياً في تقديم أفضل النتائج طوال المشروع. إن شغفه بكل جانب من جوانب التطوير يجعله شريكًا مثاليًا.",
+    name: "أحمد العلي",
+    title: "مدير شركة التقنية الحديثة",
+  },
+  {
+    quote:
+      "نورالدين بيرقدار شخص مبدع ومحترف. كان دائماً متاحاً ومستعداً لتقديم الدعم والمساعدة في كل مرحلة من مراحل المشروع. إن اهتمامه بالتفاصيل ودقته جعلت تجربتنا معه ناجحة ومثمرة.",
+    name: "ليلى السعدي",
+    title: "رئيسة تطوير الأعمال في حلول البرمجيات",
+  },
+  {
+    quote:
+      "كانت تجربة العمل مع نورالدين بيرقدار إيجابية للغاية. لقد أظهر مستوى عالٍ من الاحترافية والالتزام بتقديم حلول برمجية مبتكرة. أنصح بشدة بالعمل معه لأي شخص يبحث عن تطوير مشاريع برمجية عالية الجودة.",
+    name: "خالد الزهراني",
+    title: "مدير مشروع في شركة البرمجيات المتقدمة",
+  },
+];
+
+
+export const companies = [
+  // {
+  //   id: 1,
+  //   name: "cloudinary",
+  //   img: "/cloud.svg",
+  //   nameImg: "/cloudName.svg",
+  // },
+  // {
+  //   id: 2,
+  //   name: "appwrite",
+  //   img: "/app.svg",
+  //   nameImg: "/appName.svg",
+  // },
+  // {
+  //   id: 3,
+  //   name: "HOSTINGER",
+  //   img: "/host.svg",
+  //   nameImg: "/hostName.svg",
+  // },
+  // {
+  //   id: 4,
+  //   name: "stream",
+  //   img: "/s.svg",
+  //   nameImg: "/streamName.svg",
+  // },
+  // {
+  //   id: 5,
+  //   name: "docker.",
+  //   img: "/dock.svg",
+  //   nameImg: "/dockerName.svg",
+  // },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Freelance",
+    desc: "Assisted in the development of a web-based platform  enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - Freelance",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using Flutter .",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    href:"https://github.com/Nooraldenberkdar",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    href:"https://www.linkedin.com/in/%F0%9F%87%B5%F0%9F%87%B8noor-alden-berkdar-03b50821b?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Ba6cIV%2BYdSzWfamVu%2BNc%2B%2Fw%3D%3D"
+  },{
+    id: 3,
+    img: "/telgram.svg",
+    href:"https://t.me/nooraldenberkdar?fbclid=IwZXh0bgNhZW0CMTAAAR1y9TA1_FDnLSzer_FVHiNoD8YzdIMjZIIhHVmcCdQV6RUIZS2q5U8YFlo_aem_AWoi7xKla8oGXLcEb-2GDYt-8YT_b-WtLkKYHzm992740J3Bmmkr3mR0FfAmp9Ephf3FX8SPpk6m2eSMvpl48IMS"
+  },{
+    id: 4,
+    img: "/whatsapp.svg",
+    href:"http://wa.me/+963936553239",
+  },
+  {
+    id: 5,
+    img: "/resume.svg",
+    href:"https://drive.google.com/file/d/14ji-fdSmuN2Fd_XLNWA76ydPkcGBgkuX/view?usp=drive_link",
+  },
+ 
+];
